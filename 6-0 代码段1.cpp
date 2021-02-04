2e99fa313c57b597950006f3999a5af017b05951
GuYan1024 <chinaguyan@gmail.com>
2021-02-04 10:29:19 +0800

    第6弹更新

---
+++ 6-0 代码段1.cpp
@@ -0,0 +1,21 @@
+
+//代码段1
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    while ((i >= 5) && (i <= 10))
+        i++;
+
+    cout << i;
+
+    return 0;
+}
+
+
+
+
+
---
+++ 6-0 代码段2.cpp
@@ -0,0 +1,17 @@
+
+//代码段2
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int i = 0;
+    do
+        i++;
+    while ((i >= 5) && (i <= 10));
+
+    cout << i;
+
+    return 0;
+}
